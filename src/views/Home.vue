34f11237e7514e34a589e75856fdb15251e10ec2
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2021-09-28 05:39:27 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <BannerShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+import HeaderShayna from "@/components/HeaderShayna";
+import HeroShayna from "@/components/HeroShayna";
+import BannerShayna from "@/components/BannerShayna";
+import InstaShayna from "@/components/InstaShayna";
+import PartnerShayna from "@/components/PartnerShayna";
+import FooterShayna from "@/components/FooterShayna";
+export default {
+  name: "Home",
+  components: { HeaderShayna, HeroShayna, BannerShayna, InstaShayna, PartnerShayna, FooterShayna },
+};
+</script>
